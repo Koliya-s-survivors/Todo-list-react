958a0e1202ba464b3bc70721ceee598109ccaeda
Fouladi, Hamed <hfouladi@hq.telesens.lan>
2021-07-28 13:01:17 +0300

    rewrite state to the child component

--- src/components/counter-control/counter-control.js
+++ src/components/counter-control/counter-control.js
@@ -1,11 +1,53 @@
 import React, {Component} from 'react';
 
 class CounterControl extends Component {
+
+  state = {
+    counter: 0,
+  }
+
+  // shouldComponentUpdate(nextProps, nextState, nextContext) {
+  //   console.log(` nextContext: `, nextContext);
+  //   if ((nextState.counter %2) === 0) {
+  //     return true;
+  //   }
+  //   return false;
+  // }
+
+  incrementCounter = (data) => {
+    const newCounter = this.state.counter + 1;
+    // todo ...will be logic about positive
+    this.setState({counter: newCounter})
+    console.log(` data: `, data);
+  }
+
+  decrementCounter = () => {
+    const newCounter = this.state.counter - 1;
+    if (newCounter >= 0) {
+      this.setState({counter: newCounter})
+    }
+  }
+
+  componentDidMount() { // Выполняет какое-то действие после того как страница
+                         // первый раз отрендерилась
+    this.props.rewriteCounter(this.state.counter);
+  }
+
+  componentDidUpdate(prevProps, prevState) { // проверяет предыдущее и текущее состояние
+                                             // пропсов и стейтов
+                                            // и в случае их изменений что-то делает
+    if (prevState !== this.state) {
+      if (this.state.counter %2 === 0){
+        this.props.rewriteCounter(this.state.counter);
+      }
+    }
+  }
+
   render() {
     return (
       <div>
-        <button onClick={() => this.props.incrementCounter('Give me money!!!')}>inc +</button>
-        <button onClick={this.props.decrementCounter}>dec -</button>
+        <button onClick={() => this.incrementCounter('Give me money!!!')}>inc +</button>
+        <button onClick={this.decrementCounter}>dec -</button>
       </div>
     );
   }
--- src/components/counter/counter.js
+++ src/components/counter/counter.js
@@ -4,39 +4,20 @@ import CounterView from '../counter-view';
 import CounterControl from '../counter-control';
 
 class Counter extends Component {
+
   state = {
-    counter: 0,
+    transferCounter: null
   }
 
-  shouldComponentUpdate(nextProps, nextState, nextContext) {
-    console.log(` nextContext: `, nextContext);
-    if ((nextState.counter %2) === 0) {
-      return true;
-    }
-    return false;
-  }
-
-  incrementCounter = (data) => {
-    const newCounter = this.state.counter + 1;
-    // todo ...will be logic about positive
-    this.setState({counter: newCounter})
-    console.log(` data: `, data);
-  }
-
-  decrementCounter = () => {
-    const newCounter = this.state.counter - 1;
-    if (newCounter >= 0) {
-      this.setState({counter: newCounter})
-    }
-
+  rewriteCounter = (counter) => {
+    this.setState({ transferCounter: counter });
   }
 
   render() {
     return (
       <div>
-        <CounterView counter={this.state.counter}/>
-        <CounterControl incrementCounter={this.incrementCounter}
-                        decrementCounter={this.decrementCounter}/>
+        <CounterView counter={this.state.transferCounter}/>
+        <CounterControl rewriteCounter={this.rewriteCounter}/>
       </div>
     );
   }
